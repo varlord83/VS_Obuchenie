97b794486ba6e47a5c54546e2666478ac9ad755e
Anvar <var2@mail.ru>
2022-05-10 11:50:15 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,58 @@
+"use strict";
+// let number = 4.5;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const person = "Alex";
+
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+
+// const obj = {
+//     name: "Jone",
+//     age: 25,
+//     isMaried: false
+
+// };
+// // console.log(obj.name);
+
+// console.log(obj ["name"]);
+
+
+// let arr =['plum.png', 'orange.jpg',6,'apple.bmp' ];
+// console.log(arr[0]);
+
+// alert('Hello');
+
+// const resault = confirm ("Ты тут"); //confirm -что то спрашиваем у пользователя
+// console.log(resault);
+
+// const answer = +prompt("Вам есть 18", "18"); //prompt - спросили для получения 
+// //ответа получаем string если поставить + получим number
+// console.log(answer + 5); //typeof- определяем тип данных
+
+// const answer = [];
+// answer[0] = prompt("Как тебя зовут", "Ваше имя");
+// answer[1] = prompt("Какая у тебя фамилия", "Ваша фамилия");
+// answer[2] = prompt("Сколько тебе лет", "25");
+
+// document.write (answer);
+
+//ИНТЕРПАЛЯЦИЯ Добавление в конце с помощью кривых ковычек ``
+
+// const category = "toys";
+// console.log(`https://interior.kg/${category}/5`);
+
+// const user ="Ivan";
+// alert(`Привет, ${user}`)
+const name1 = prompt("Как Вас зовут", "");
+alert(`Ваше имя, ${name1}`);
+document.write (name1);
+
